7aefac41b11b2be5c665882ef3a0f5186fda428e
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-09-04 10:30:26 +0200

    run script in web

---
+++ web/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -m
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR && python3 -m http.server 8888 &
+
+xdg-open http://0.0.0.0:8888/
+
+fg
+
